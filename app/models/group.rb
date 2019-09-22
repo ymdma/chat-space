df5d8ebda44068ff1c0a7f51371a850d500eb8a9
ymdma <m.yamada1985@gmail.com>
2019-09-22 16:30:55 +0900

    190922_addMessageFunction

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,25 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
+
+# 三項演算子を用いない場合
+# def show_last_message
+#   if (last_message = messages.last).present?
+#     if last_message.content?
+#      last_message.content
+#     else
+#       '画像が投稿されています'
+#     end
+#   else
+#     'まだメッセージはありません。'
+#   end
