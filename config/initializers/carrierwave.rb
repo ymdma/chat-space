c389d7a987d702098905812dc61532fcc78bcc9e
ymdma <m.yamada1985@gmail.com>
2019-09-28 18:04:55 +0900

    carriewave-rb_fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'new-upload1'
-  config.asset_host = 'ap-northeast-1.amazonaws.com/new-upload1'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/new-upload1'
 end
